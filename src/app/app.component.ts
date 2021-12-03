dbdbb4d887be8b746c6c0a5c3070c32ea6873d9b
smkjain000 <smkjain000@gmail.com>
2021-12-03 21:26:26 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',                         //for user-defined tags
+  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-app';
+  id:number=100;      //data type declaration, not possible in javascript
+  name:string="Samyak"
+  age:number=21;
+  result:boolean=true
 }
